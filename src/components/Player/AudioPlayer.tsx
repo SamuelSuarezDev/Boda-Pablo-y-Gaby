092fdb818bc96788bf2ebffcf4fc491037a155c0
Daniel Suarez <danielsuarez3114@gmail.com>
2022-12-13 15:19:46 -0500

    rebase

--- src/components/Player/AudioPlayer.tsx
+++ src/components/Player/AudioPlayer.tsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
 import AudioControls from "./AudioControls";
 import "./styles.css";
+<<<<<<< HEAD
 // @ts-ignore
+=======
+// @ts-ignore  
+>>>>>>> 1c24baf (player component)
 import perfect from "../../assets/audio.mp3";
 
 const AudioPlayer = () => {
@@ -13,7 +17,11 @@ const AudioPlayer = () => {
   const intervalRef = useRef();
 
   // Destructure for conciseness
+<<<<<<< HEAD
   const { duration }: any = audioRef.current;
+=======
+  const { duration } = audioRef.current;
+>>>>>>> 1c24baf (player component)
 
   const currentPercentage = duration
     ? `${(trackProgress / duration) * 100}%`
@@ -22,6 +30,7 @@ const AudioPlayer = () => {
     -webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #fff), color-stop(${currentPercentage}, #777))
   `;
 
+<<<<<<< HEAD
   const startTimer = (): any => {
     // Clear any timers already running
     clearInterval(intervalRef.current);
@@ -39,6 +48,22 @@ const AudioPlayer = () => {
     // @ts-ignore
     audioRef.current.currentTime = value;
     // @ts-ignore
+=======
+  const startTimer = ():any => {
+    // Clear any timers already running
+    clearInterval(intervalRef.current);
+// @ts-ignore  
+    intervalRef.current = setInterval(() => {
+        setTrackProgress(audioRef.current.currentTime);
+        // @ts-ignore  
+    }, [1000]);
+  };
+
+  const onScrub = (value:any) => {
+    // Clear any timers already running
+    clearInterval(intervalRef.current);
+    audioRef.current.currentTime = value;
+>>>>>>> 1c24baf (player component)
     setTrackProgress(audioRef.current.currentTime);
   };
 
@@ -52,11 +77,17 @@ const AudioPlayer = () => {
 
   useEffect(() => {
     if (isPlaying) {
+<<<<<<< HEAD
       // @ts-ignore
       audioRef.current.play();
       startTimer();
     } else {
       // @ts-ignore
+=======
+      audioRef.current.play();
+      startTimer();
+    } else {
+>>>>>>> 1c24baf (player component)
       audioRef.current.pause();
     }
   }, [isPlaying]);
@@ -64,7 +95,10 @@ const AudioPlayer = () => {
   useEffect(() => {
     // Pause and clean up on unmount
     return () => {
+<<<<<<< HEAD
       // @ts-ignore
+=======
+>>>>>>> 1c24baf (player component)
       audioRef.current.pause();
       clearInterval(intervalRef.current);
     };
@@ -73,7 +107,14 @@ const AudioPlayer = () => {
   return (
     <div className="audio-player">
       <div className="track-info">
+<<<<<<< HEAD
         <AudioControls isPlaying={isPlaying} onPlayPauseClick={setIsPlaying} />
+=======
+        <AudioControls
+          isPlaying={isPlaying}
+          onPlayPauseClick={setIsPlaying}
+        />
+>>>>>>> 1c24baf (player component)
         <input
           type="range"
           value={trackProgress}
@@ -84,7 +125,11 @@ const AudioPlayer = () => {
           onChange={(e) => onScrub(e.target.value)}
           onMouseUp={onScrubEnd}
           onKeyUp={onScrubEnd}
+<<<<<<< HEAD
           style={{ background: trackStyling, width: "80%", marginLeft: "10px", border: "1px solid red" }}
+=======
+          style={{ background: trackStyling, width: '80%', marginLeft: '10px' }}
+>>>>>>> 1c24baf (player component)
         />
       </div>
     </div>
