adc27576e39172694263c9bb54699973c07731ca
Daniel Suarez <danielsuarez3114@gmail.com>
2022-12-12 22:57:08 -0500

    player component

---
+++ src/components/Player/Player.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactAudioPlayer from "react-audio-player";
+import ReactPlayer from "react-player";
+import AudioPlayer from './AudioPlayer';
+// @ts-ignore  
+import perfect from "../../assets/audio.mp3";
+
+export const Player = () => {
+
+    
+  return (
+    <AudioPlayer />
+  );
+};
