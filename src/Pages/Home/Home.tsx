32f7a8d2ac824b7e5f1e34fee028607deec5b84d
Samuel Suarez <samuelsuarez.lhs@gmail.com>
2022-12-12 20:50:53 -0500

    counter

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -3,6 +3,14 @@ export const Home = () => {
   return (
     <>
       <Hero />
+      <div className="song-container">
+        <p className="title">Nuestra canción</p>
+        <p className="subtitle">Dale play a nuestra cancion</p>
+      </div>
+      <div className="history">
+        <p className="title">Nuestra historia</p>
+        <p className="subtitle"></p>
+      </div>
     </>
   );
 };
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,29 @@
 import "./styled.css";
+import { useState, useRef } from "react";
+function Hero() {
+  const [timerDays, setTimerDays] = useState(0);
+  const [timerHours, setTimerHours] = useState(0);
+  const [timerMinutes, setTimerMinutes] = useState(0);
+  const [timerSeconds, setTimerSeconds] = useState(0);
+  const countDate = new Date("01/15/2023 4:00 PM").getTime();
+  setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-const Hero = () => {
+    if (distance < 0) {
+    } else {
+      setTimerDays(days);
+      setTimerHours(hours);
+      setTimerMinutes(minutes);
+      setTimerSeconds(seconds);
+    }
+  });
   return (
     <>
       <div className="container">
@@ -16,7 +39,6 @@ const Hero = () => {
             alt=""
             className="background two"
           />
-
           <img
             src="https://i.postimg.cc/vmQs9N9v/Whats-App-Image-2022-12-02-at-10-58-13-AM.jpg"
             alt=""
@@ -32,8 +54,30 @@ const Hero = () => {
             Te invitamos a compartir la alegria de unir nuestras vidas
           </p>
           <p className="day">Para este día faltan:</p>
-          {/*       <CountDownTimer />
-           */}{" "}
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-around",
+            }}
+          >
+            <div>
+              <p>{timerDays}</p>
+              <span>Días</span>
+            </div>
+            <div>
+              <p>{timerHours}</p>
+              <span>Horas</span>
+            </div>
+            <div>
+              <p>{timerMinutes}</p>
+              <span>Minutos</span>
+            </div>
+            <div>
+              <p>{timerSeconds}</p>
+              <span>Segundos</span>
+            </div>
+          </div>
         </div>
       </div>
       <img
@@ -43,6 +87,6 @@ const Hero = () => {
       />
     </>
   );
-};
+}
 
 export default Hero;
